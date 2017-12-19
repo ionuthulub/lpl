a6b7fa129188e824c1d873a1c77cf9d754db23c1
Ionut Hulub <ionut.hulub@gmail.com>
2017-12-19 19:19:58 +0200

    integration with GATE

--- txt2xml.py
+++ txt2xml.py
@@ -1,11 +1,13 @@
 import codecs
 import sys
+import json
 
 import nltk
+import gate
 
 
 def get_corpus():
-    with codecs.open(sys.argv[1], 'r', encoding='utf-8') as fin:
+    with codecs.open('corpus.txt', 'r', encoding='utf-8') as fin:
         return fin.read()
 
 
@@ -19,13 +21,16 @@ def generate_xml(tokens):
 </DOCUMENT>""".format(partial_result)
 
 
-def main():
-    corpus = get_corpus()
+@gate.executable
+def main(document):
+    # corpus = get_corpus()
+    corpus = document.text
     tokens = nltk.word_tokenize(corpus)
     xml = generate_xml(tokens)
     with codecs.open('output.xml', 'w', encoding='utf-8') as fout:
         fout.write(xml)
+	return document
 
 
 if __name__ == '__main__':
-    main()
+    main.start()
--- xml2txt.py
+++ xml2txt.py
@@ -1,12 +1,13 @@
 import codecs
 import sys
 
+import gate
 from nltk.tokenize import moses
 import xml
 
 
 def get_tokens():
-    tree = xml.etree.ElementTree.parse(sys.argv[1])
+    tree = xml.etree.ElementTree.parse('output.xml')
     root = tree.getroot()
     tokens = []
     for w in root.iter('w'):
@@ -14,12 +15,14 @@ def get_tokens():
     return tokens
 
 
-def main():
+@gate.executable
+def main(document):
     tokens = get_tokens()
     output = moses.MosesDetokenizer().detokenize(tokens, return_str=True)
     with codecs.open('output.txt', 'w', encoding='utf-8') as fout:
         fout.write(output)
+	return document
 
 
 if __name__ == '__main__':
-    main()
+    main.start()
